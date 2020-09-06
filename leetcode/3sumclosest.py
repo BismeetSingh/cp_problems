c2f58fe15744337f87019ee18a9fde8e4d5fe5b5
bismeetsingh <bismeetsingh352@gmail.com>
2020-09-06 07:48:36 +0530

    Added 3sum closest to target

--- leetcode/3sumclosest.py
+++ leetcode/3sumclosest.py
@@ -1,29 +1,35 @@
 #Three sum closest to target.
 #The idea is to keep track of the minimum distance from the target value.
-def solve(a,t):
-    
+
+def threeSumClosest(a, t):
+    if not a:
+        return 0
     a.sort()
     i=0
-    ans=set()
+    ans=0
+    indices=None
+    value=None
     diff=float("inf")
     while(i<len(a)):
         j=i+1
         k=len(a)-1
         while(j<k):
             sum=a[j]+a[k]+a[i]
-            if sum==t:
-                tmp=(a[i],a[j],a[k])
-                ans.add(tmp)
-                
+            if abs(t-sum)<abs(diff):
+                diff=abs(sum-t)
+                ans=sum
+                indices=(i,j,k)
+                value=(a[i],a[j],a[k])
             if sum>t:
                 k-=1
             else:
                 j+=1
         
         i+=1
-    return ans
+    return ans,indices,value
+
 
 
 a=[-7,14,-7,8,-4,-4,-4,-4,-4]
-t=0
-print(solve(a,t))
+t=89
+print(threeSumClosest(a,t))
