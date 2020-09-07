1a7ac366cc20adb996147d0c387c9465c2ff2c4d
bismeetsingh <bismeetsingh352@gmail.com>
2020-09-07 19:07:00 +0530

    Added clone singly linked list with random pointer

--- leetcode/clonoelinkedlistwithrandompointer.py
+++ leetcode/clonoelinkedlistwithrandompointer.py
@@ -1,6 +1,7 @@
 #use hashmaps
 #make a deepcopy of the linked list.
 #the idea is to make usual singly first ans then copy over the randoms from the hashmap.
+#Question link : https://leetcode.com/problems/copy-list-with-random-pointer/
 class Solution:
     def copyRandomList(self, head: 'Node') -> 'Node':
         
