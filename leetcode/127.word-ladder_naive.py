a08fc1f4a8bbad0ade1fbbae5205ae08de774692
bismeetsingh <bismeetsingh352@gmail.com>
2020-09-09 22:26:33 +0530

    Added Solutions

--- leetcode/127.word-ladder_naive.py
+++ leetcode/127.word-ladder_naive.py
@@ -6,20 +6,22 @@
 #https://leetcode.com/problems/word-ladder/
 #This is the most naive  approach
 # @lc code=start
+from collections import deque
 class Solution:
     def ladderLength(self, beginWord: str, endWord: str, wordList: List[str]) -> int:
           
         wordList=set(wordList)
         if endWord not in wordList:
             return 0
-        queue=[beginWord]
+        queue=deque()
+        queue.append(beginWord)
         level=1
         s="abcdefghijklmnopqrstuvwxyz"
         while queue:
             size=len(queue)
             for i in range(size):
                 #Pop the current word
-                current_word=queue.pop(0)
+                current_word=queue.popleft()
                 l=list(current_word)
                 for i in range(len(l)):
                     ch=l[i]
