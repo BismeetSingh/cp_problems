865083056ac547ae3b0d9851c250c787469b1ee2
bismeetsingh <bismeetsingh352@gmail.com>
2020-09-16 18:01:16 +0530

    Added multiple solutions

--- leetcode/xor_pairs.py
+++ leetcode/xor_pairs.py
@@ -12,6 +12,7 @@ class TrieNode:
         self.maximum=0
 
     def insert(self,word):
+        #Simple trie insertion.
         current=self.chardict
         for c in word:
             if c not in current:
@@ -24,6 +25,8 @@ class TrieNode:
         current=self.chardict
         s=""
         for c in word:
+            #if we get a 0,we want a 1 because 0 XOR 1 would bring us close to 1. We want 1 in the most significant bits
+            #so as to get the largest possible number.
             if c=="0":
                 toSearch="0"
                 complement="1"
@@ -41,6 +44,7 @@ class TrieNode:
         print(s)
         a=int(word,2)
         b=int(s,2)
+        #We take a maximum of the current xor number and the number we passed in the function.
         self.maximum=max(self.maximum,a^b)
             # print(current)
             # for k,v in current.items():
@@ -53,16 +57,19 @@ class TrieNode:
 trie = TrieNode()
 arr=[3,10,5,25,2,8]
 bits=[]
+#We find binary  representation of each number and build a trie out of it.
 for i in arr:
     binary=str(bin(i))
     binary=binary.replace("0b","")
-    while len(binary)<5:
+    while len(binary)<32:
         binary="0"+binary
     bits.append(binary)
 
 # print(bits)
+#Insert in trie.
 for i in bits:
     trie.insert(i)
+#Search all binary nums in trie.
 for i in bits:
     trie.search(i)
 
